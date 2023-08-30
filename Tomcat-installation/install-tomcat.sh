fd28b3eae8807135ca4bf15bbf97c9de281d7919
Valshem3 <140100043+Valshem3@users.noreply.github.com>
2023-08-30 14:48:37 -0400

    Update install-tomcat.sh
    Added more comment for easy understanding

--- Tomcat-installation/install-tomcat.sh
+++ Tomcat-installation/install-tomcat.sh
@@ -22,7 +22,7 @@ sudo chown ec2-user -R /opt/tomcat9
 ### start tomcat
 sh /opt/tomcat9/bin/startup.sh
 # create a soft link to start and stop tomcat
-# This will enable us to manage tomcat as a service
+# This will enable us to manage tomcat as a service on any server of choice
 sudo ln -s /opt/tomcat9/bin/startup.sh /usr/bin/starttomcat
 sudo ln -s /opt/tomcat9/bin/shutdown.sh /usr/bin/stoptomcat
 starttomcat
